a346c69934d93b308766181aa87bfc768f9182d4
AriHyuk <coygalang990@gmail.com>
2025-06-24 23:11:18 +0700

    deploy

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,8 +16,10 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'admin',
+            'email' => 'admin@gmail.com',
+            'password' => bcrypt('arixyz123'), // Ensure to hash the password
+
         ]);
     }
 }
