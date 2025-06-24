19f487e7e443bc307cdb5dcc7168e087f924b99e
AriHyuk <coygalang990@gmail.com>
2025-06-24 23:00:20 +0700

    deploy

--- start.sh
+++ start.sh
@@ -3,3 +3,9 @@
 php artisan storage:link || true
 php artisan migrate --force || true
 php artisan serve --host=0.0.0.0 --port=${PORT:-8000}
+
+php artisan config:clear
+php artisan cache:clear
+php artisan route:clear
+php artisan view:clear
+
